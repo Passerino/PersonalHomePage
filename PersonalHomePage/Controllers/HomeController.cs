7556ff6e0f5d675d1fc903ebbb6a550b4163626c
Bartosz Wróbel <bartosz.d.wrobel@gmail.com>
2012-11-08 21:18:24 +0000

    Index change on app harbor

--- PersonalHomePage/Controllers/HomeController.cs
+++ PersonalHomePage/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace PersonalHomePage.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Gekon, Nie daj się!";
 
             return View();
         }
